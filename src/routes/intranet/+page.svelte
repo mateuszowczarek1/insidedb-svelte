059e736f0a7a800f95f92afabcce86f73709283d
mateuszowczarek <mateuszowczarek@tutamail.com>
2024-12-22 11:22:03 +0100

    :art: forgot to add search component to auth-intranet-view | now should be fine

--- src/routes/intranet/+page.svelte
+++ src/routes/intranet/+page.svelte
@@ -3,9 +3,10 @@
 	import ListCategories from "../../components/categories/listCategories.svelte";
 	import ListRecipes from "../../components/recipes/listRecipes.svelte";
     import { page } from '$app/state';
+	import Search from "../../components/search/search.svelte";
     let isAdmin = page.data.session?.user.userRole;
 </script>
-
+<Search />
 <ListCategories />
 <ListRecipes />
 <!-- TODO: Conditional rendering -->
